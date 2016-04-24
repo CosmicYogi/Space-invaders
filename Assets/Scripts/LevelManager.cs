15a2777652a46e13db08416c9feb21e5d019daf5
Mitesh Soni <mitesh_soni@rocketmail.com>
2016-04-24 23:54:13 +0530

    Worked on players controller,Mouse & keyboard movement completed

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void  loadLevel(string name){
+		UnityEngine.SceneManagement.SceneManager.LoadScene (name);
+	}
+	public void quitGame(){
+		Application.Quit();
+	}
+
+	public void LoadNextLevel(){
+		UnityEngine.SceneManagement.SceneManager.LoadScene (UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex + 1);
+
+	}
+		
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+
+	enum inputMethods {Mouse,Keyboard};
+	inputMethods currentinput; 
+	float playerShip = 0f;
+	float speed = 12.0f;
+	void Start () {
+		currentinput = inputMethods.Mouse;
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentinput == inputMethods.Keyboard) {
+			MoveWithKeyboard ();
+			if (Input.GetMouseButton (0)) {
+				currentinput = inputMethods.Mouse;
+			}
+		}
+		if (currentinput == inputMethods.Mouse) {
+			MoveWithMouse ();
+			if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow)
+				|| Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow)){
+				print("Fuck");
+				currentinput = inputMethods.Keyboard;
+			}
+		}
+	}
+	void MoveWithKeyboard(){
+		float positionX = this.transform.position.x;
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			this.transform.position += Vector3.left * speed * Time.deltaTime;
+			float posX = Mathf.Clamp(this.transform.position.x,0,12.4f);
+			this.transform.position = new Vector2(posX,this.transform.position.y);
+			print (this.transform.position.x);
+		}
+		else if (Input.GetKey (KeyCode.RightArrow)) {
+			this.transform.position += Vector3.right * speed * Time.deltaTime;
+			float posX = Mathf.Clamp (this.transform.position.x, 0, 12.4f);
+			this.transform.position = new Vector2 (posX, this.transform.position.y);
+			print (this.transform.position.x);
+		}
+	}
+	void MoveWithMouse(){
+		playerShip = Input.mousePosition.x / Screen.width * 13;
+		this.transform.position = new Vector2 (Mathf.Clamp(playerShip,0f,12.5f), this.transform.position.y);
+	}
+}
