3988b4377093d0bd1343bfd59246288f8601b85b
Mitesh Soni <mitesh_soni@rocketmail.com>
2016-04-28 17:42:59 +0530

    Enemies created, Laser for shooting enemies create

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+	public float speed = 1;
+	public GameObject enemyPrefab;
+	float center;
+	float min;
+	float max;
+	float padding = 0.5f;
+	Vector3 worldPointCoordinates;
+	public Vector3 boxCenter;
+	public float width = 10;
+	public float height = 5;
+	bool movingRight = true;
+	// Use this for initialization
+	void Start () {
+		Vector3 distance = this.transform.position - Camera.main.transform.position;
+		distance.x = Mathf.Abs (distance.x);
+		worldPointCoordinates = Camera.main.ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, distance.x));
+		center = worldPointCoordinates.x; // 
+
+		min = Camera.main.ViewportToWorldPoint (new Vector3 (0f, 0f, distance.x)).x + padding;
+		max = Camera.main.ViewportToWorldPoint (new Vector3 (1f, 0f, distance.x)).x - padding ;
+		print (min + " and " + max);
+
+		foreach (Transform child in transform) {
+			GameObject enemy = Instantiate (enemyPrefab, child.transform.position , Quaternion.identity) as GameObject;
+			enemy.transform.parent = child;
+		}
+	}
+
+	void OnDrawGizmos(){
+		Gizmos.DrawWireCube (boxCenter, new Vector3 (width, height, 0));
+	}
+	// Update is called once per frame
+	void Update () {
+
+		foreach (Transform child in transform) {
+			if (child.transform.position.x >= max) {
+				movingRight = false;
+			} else if (child.transform.position.x <= min) {
+				movingRight = true;
+			}
+		}
+
+		switch (movingRight) {
+		case true:
+			MoveRight ();
+			break;
+
+		case false:
+			MoveLeft ();
+			break;
+		}
+			
+	}
+
+	void MoveLeft(){
+		// For moving left.
+		transform.position -= new Vector3 (speed * Time.deltaTime, 0f, 0f); 
+	}
+	void MoveRight(){
+		//For moving right.
+		transform.position += new Vector3 (speed * Time.deltaTime, 0f, 0f);
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,7 +10,10 @@ public class PlayerController : MonoBehaviour {
 	float max;
 	float padding = 0.5f;
 	float playerShip = 0f;
-	float speed = 12.0f;
+	public float speed = 12.0f;
+	public float laserSpeed = 15f;
+	public float fireRate = 0.4f;
+	public Transform laser;
 	void Start () {
 		currentinput = inputMethods.Mouse;
 		float distance = transform.position.z - Camera.main.transform.position.z;
@@ -31,7 +34,8 @@ public class PlayerController : MonoBehaviour {
 		if (currentinput == inputMethods.Mouse) {
 			MoveWithMouse ();
 			if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow)
-				|| Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow)){
+				|| Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow)
+				|| Input.GetKeyDown(KeyCode.Space)){
 				print("Fuck");
 				currentinput = inputMethods.Keyboard;
 			}
@@ -43,17 +47,33 @@ public class PlayerController : MonoBehaviour {
 			this.transform.position += Vector3.left * speed * Time.deltaTime;
 			float posX = Mathf.Clamp(this.transform.position.x,min,max);
 			this.transform.position = new Vector2(posX,this.transform.position.y);
-			print (this.transform.position.x);
+			//print (this.transform.position.x);
 		}
 		else if (Input.GetKey (KeyCode.RightArrow)) {
 			this.transform.position += Vector3.right * speed * Time.deltaTime;
 			float posX = Mathf.Clamp (this.transform.position.x,min, max);
 			this.transform.position = new Vector2 (posX, this.transform.position.y);
-			print (this.transform.position.x);
+			//print (this.transform.position.x);
+		}
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			InvokeRepeating ("Fire", 0.000001f, fireRate);// here second parameter is not 0 because zero sometime give errors so very very small number.
+		}
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			CancelInvoke ();
 		}
 	}
 	void MoveWithMouse(){
+		if (Input.GetMouseButtonDown (0)) {
+			InvokeRepeating ("Fire", 0.00000f, fireRate);
+		}
+		if (Input.GetMouseButtonUp (0)) {
+			CancelInvoke ();
+		}
 		playerShip = Input.mousePosition.x / Screen.width * 13;
 		this.transform.position = new Vector2 (Mathf.Clamp(playerShip,min,max), this.transform.position.y);
+	}
+
+	void Fire(){
+		GameObject laserBeam = Instantiate (laser, transform.position, Quaternion.identity) as GameObject;
 	}
 }
