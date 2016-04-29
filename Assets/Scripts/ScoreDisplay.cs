d173efc373223a3924ade01c03e8ab303bc0d73c
Mitesh Soni <mitesh_soni@rocketmail.com>
2016-04-29 18:40:41 +0530

    Menu system polished

---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class ScoreDisplay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Text> ().text = "Your Score : " + ScoreKeeper.score;
+		ScoreKeeper.Reset ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
